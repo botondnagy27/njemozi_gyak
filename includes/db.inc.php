7ee079f8de7c06693863fb06aba0fe2da61b9479
botondnagy27 <89258559+botondnagy27@users.noreply.github.com>
2026-04-12 01:02:33 +0200

    Update db.inc.php

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -2,9 +2,9 @@
 try {
     // Élesítéskor csak EZEKET az adatokat kell majd átírni!
     $host = 'localhost';
-    $dbname = 'gyakorlat7'; // A tárhelyen ez lesz az 'adatb'
-    $user = 'root';         // A tárhelyen ez lesz az 'adatbf'
-    $pass = '';             // A tárhelyen ez lesz a jelszó '****'
+    $dbname = 'fi4exm'; // A tárhelyen ez lesz az 'adatb'
+    $user = 'fi4exm';         // A tárhelyen ez lesz az 'adatbf'
+    $pass = 's3G@rhUTd8Lz@Z!';             // A tárhelyen ez lesz a jelszó '****'
 
     $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
     $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
